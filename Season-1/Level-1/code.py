9bbc0cc46ec5b661d34735e14fdcf28594a1b23e
Vicodina <vicodina01@gmail.com>
2024-03-26 18:30:12 +0000

    Secure Code Game

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,22 +12,29 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
+    net1 = Decimal('0')
+    net2 = Decimal('0')
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if item.amount >=-100000 and item.amount <=100000:
+                net1 += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if type(item.quantity) is int and item.quantity > 0 and item.quantity <=100 and item.amount > 0 and item.amount <=100000:
+                net2 += Decimal(str(item.amount * item.quantity))
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    if net1 > 1000000 or net2 > 1000000:
+        return "Total amount payable for an order exceeded"
+
+    if net1 != net2:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net1-net2)
     else:
         return "Order ID: %s - Full payment received!" % order.id
